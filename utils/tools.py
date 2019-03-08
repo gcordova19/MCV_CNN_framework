ddffa7bb921a4ecc75a4de174a51cb9ab9d50ade
grupo09 <gaby19_956@hotmail.com>
2019-03-08 20:29:14 +0100

    change default changelist2

--- utils/tools.py
+++ utils/tools.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 as cv
 import matplotlib
-import StringIO
+import io
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
@@ -23,7 +23,7 @@ def confm_metrics2image(conf_matrix,names=None):
             else:
                 conf_matrix[i][j] = (conf_matrix[i][j]) / float(sum_row)
 
-    img = StringIO.StringIO()
+    img = io.StringIO()
     plt.ioff()
     plt.cla()
     plt.clf()
@@ -36,7 +36,7 @@ def confm_metrics2image(conf_matrix,names=None):
     plt.title('Confusion Matrix')
 
     plt.xticks(range(nLabels), plt_names, rotation=90)
-    ystick = zip(plt_names, [conf_matrix[i][i] for i in range(nLabels)])
+    ystick = list(zip(plt_names, [conf_matrix[i][i] for i in range(nLabels)]))
     ystick_str = [str(ystick[i][0]) + '(%.2f)' % ystick[i][1] for i in range(nLabels)]
 
     plt.yticks(range(nLabels), ystick_str)
@@ -49,11 +49,11 @@ def confm_metrics2image(conf_matrix,names=None):
     plt.savefig(img, format='png')
     img.seek(0)
 
-    data = np.asarray(bytearray(img.buf), dtype=np.uint8)
+    data = np.fromstring(img.getvalue(), dtype=np.uint8)
     img = cv.imdecode(data, cv.IMREAD_UNCHANGED)[:, :, 0:3]
-    img = img[..., ::-1]
+    return img[..., ::-1]
 
-    return img
+
 
 def save_prediction(output_path, predictions, names):
     for img in range(len(names)):
