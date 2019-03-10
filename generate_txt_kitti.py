57d67b325ebb98e37021bbce20d8600c7a497fb9
grupo09 <gaby19_956@hotmail.com>
2019-03-10 12:36:05 +0100

    now prints labels, map_labels and num_classes

--- generate_txt_kitti.py
+++ generate_txt_kitti.py
@@ -5,8 +5,10 @@ txt_dir = "/home/grupo09/M5/marc/MCV_CNN_framework/KITTI/"
 
 ground_truth_train = ""
 count_train = 0
+labels = []
 with open(txt_dir+"train_images.txt", "w+") as f:
     for class_dir in listdir(dataset + "train/"):
+        labels.append(class_dir)
         for image in listdir(dataset + "train/" + class_dir + "/"):
             f.write(dataset + "train/" + class_dir + "/" + image+'\n')
             ground_truth_train += class_dir +"\n"
@@ -50,3 +52,18 @@ print("generated valid_images.txt and valid_gt.txt at", txt_dir)
 print("num of valid samples:", count_valid)
 print("generated test_images.txt and test_gt.txt at", txt_dir)
 print("num of test samples:", count_test)
+
+print("-----------\nlabels:")
+print(labels)
+
+map_labels = {}
+count_labels = 0
+for label in labels:
+    map_labels[label] = count_labels
+    count_labels+=1
+
+print("-----------\nmap_labels:")
+print(map_labels)
+
+print("-----------\nnum_classes:")
+print(len(labels))
